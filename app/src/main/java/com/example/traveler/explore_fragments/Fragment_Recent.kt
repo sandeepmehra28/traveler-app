4901dbf3ce0ecc2ffdb586be5f8b966fc3894307
sandeep mehra <sandeepmehra2259@gmail.com>
2025-11-03 12:27:36 +0530

    Adding new files..

---
+++ app/src/main/java/com/example/traveler/explore_fragments/Fragment_Recent.kt
@@ -0,0 +1,6 @@
+package com.example.traveler.explore_fragments
+import androidx.fragment.app.Fragment
+import com.example.traveler.R
+
+class Fragment_Recent : Fragment(R.layout.fragment_recent){
+}
--- app/src/main/java/com/example/traveler/fragments/ExploreFragment.kt
+++ app/src/main/java/com/example/traveler/fragments/ExploreFragment.kt
@@ -5,56 +5,75 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.traveler.R
+import com.example.traveler.explore_fragments.Fragment_Popular
+import com.example.traveler.explore_fragments.Fragment_Rating
+import com.example.traveler.explore_fragments.Fragment_Recent
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var popularTextView: TextView
+    private lateinit var ratingTextView: TextView
+    private lateinit var recentTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExploreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExploreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize views
+        popularTextView = view.findViewById(R.id.explore_popular_tv)
+        ratingTextView = view.findViewById(R.id.explore_rating_tv)
+        recentTextView = view.findViewById(R.id.explore_recent_tv)
+
+        // Load default fragment
+        replaceFrameWithFragment(Fragment_Popular())
+        highlightSelectedTab(popularTextView)
+
+        // Click listeners for each tab
+        popularTextView.setOnClickListener {
+            replaceFrameWithFragment(Fragment_Popular())
+            highlightSelectedTab(popularTextView)
+        }
+
+        ratingTextView.setOnClickListener {
+            replaceFrameWithFragment(Fragment_Rating())
+            highlightSelectedTab(ratingTextView)
+        }
+
+        recentTextView.setOnClickListener {
+            replaceFrameWithFragment(Fragment_Recent())
+            highlightSelectedTab(recentTextView)
+        }
+    }
+
+    private fun replaceFrameWithFragment(fragment: Fragment) {
+        val transaction = childFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    /**
+     * Highlight the selected tab and reset others
+     */
+    private fun highlightSelectedTab(selectedTextView: TextView) {
+        val defaultColor = ContextCompat.getColor(requireContext(), R.color.black)
+        val selectedColor = ContextCompat.getColor(requireContext(), R.color.blue_500) // Change this color as you like
+
+        // Reset all to default
+        popularTextView.setTextColor(defaultColor)
+        ratingTextView.setTextColor(defaultColor)
+        recentTextView.setTextColor(defaultColor)
+
+        // Highlight the selected one
+        selectedTextView.setTextColor(selectedColor)
     }
 }
